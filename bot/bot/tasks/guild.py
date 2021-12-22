901fd0848eb3c6a74fd0a8c7df843312e644b452
unrealintegers <47798345+unrealintegers@users.noreply.github.com>
2021-12-22 15:10:18 +1100

    setup guild tracking

---
+++ bot/bot/tasks/guild.py
@@ -0,0 +1,154 @@
+import asyncio
+import heapq
+from datetime import datetime as dt
+from datetime import timedelta as td
+from typing import Optional
+
+import aiocron
+import requests
+from pytz import utc
+
+from ..bot import EYESBot, BotTask
+from ..utils.wynn import GuildMember
+
+
+# TODO: GuildListUpdater should also trigger an update for GuildUpdater
+class GuildListUpdater(BotTask):
+    def __init__(self, bot: EYESBot):
+        super().__init__(bot)
+
+        self.update().start()
+
+    def path(self):
+        return self.bot.db.child('wynncraft').child('guilds')
+
+    def update(self):
+        @aiocron.crontab("0 * * * *", start=False, tz=utc)
+        async def wrapper():
+            response = requests.get("https://api.wynncraft.com/public_api.php?action=guildList")
+
+            if not response.ok:
+                self.bot.logger.error("Failed to fetch from Wynn API!")
+                return
+            else:
+                response = response.json()
+
+            try:
+                existing_guilds = set(map(lambda x: x.key(), self.path().get().each()))
+            except TypeError:
+                existing_guilds = set()
+
+            guilds = response['guilds']
+            one_day = td(days=1).total_seconds()
+            guilddict = {g: {'name': g, 'interval': one_day, 'no_diff_days': 0, 'next_update': 0}
+                         for g in guilds if g not in existing_guilds}
+
+            self.path().update(guilddict)
+
+        return wrapper
+
+
+class GuildUpdater(BotTask):
+    """A priority-queue-based implementation to update guild details
+       with exponential backoff strategy and rate limit handling"""
+
+    def __init__(self, bot):
+        super().__init__(bot)
+
+        self.pq = []
+        self.build_pq()
+
+        asyncio.create_task(self.next())
+
+    def guild_path(self):
+        return self.bot.db.child('wynncraft').child('guilds')
+
+    def prefix_path(self):
+        return self.bot.db.child('wynncraft').child('prefixes')
+
+    async def next(self):
+        if self.pq:  # is not empty
+            # We check that it is in fact time to update the smallest item
+            if dt.utcnow().timestamp() > self.pq[0][0]:
+                # Gets the first element and updates it
+                _, guild_name = heapq.heappop(self.pq)
+                next_update = self.update_guild(guild_name)
+
+                # Re-adds it back to the queue with the scheduled next update
+                heapq.heappush(self.pq, (next_update, guild_name))
+
+        # 1 request per 3s
+        await asyncio.sleep(3)
+        asyncio.create_task(self.next())
+
+    def build_pq(self):
+        guilds = self.guild_path().get().each()
+        for guild in guilds:
+            guild_name = guild.key()
+            timestamp = guild.val()['next_update']
+            heapq.heappush(self.pq, (timestamp, guild_name))
+
+    @staticmethod
+    def calc_next_interval(interval, no_diff_days, num_changes) -> td:
+        MIN_INTERVAL = td(hours=6)
+        MAX_INTERVAL = td(days=8)
+
+        # If diff = 0 for more than 5 days, increase interval
+        if num_changes == 0:
+            if interval < td(days=1) and no_diff_days >= 4:
+                interval = td(days=1)
+            else:
+                interval *= 2
+        elif num_changes != 0:
+            if interval > td(days=1):
+                interval = td(days=1)
+            elif num_changes > 1:
+                interval /= 2
+
+        interval = max(min(interval, MAX_INTERVAL), MIN_INTERVAL)
+        return interval
+
+    def update_guild(self, guild_name, update_prefix=False) -> Optional[float]:
+        """Fetches 1 guild from the API and updates it."""
+        self.bot.logger.info(f"Updating Guild {guild_name}.")
+
+        url = f"https://api.wynncraft.com/public_api.php?action=guildStats&command={guild_name}"
+        response = requests.get(url)
+
+        if not response.ok:
+            self.bot.logger.error(f"Failed to fetch from {url}.")
+            return
+        else:
+            response = response.json()
+
+        # We grab the prefix and additionally add it to a prefix path for faster lookups
+        # We only update the prefix when the guild is new as you can't change guild prefixes
+        if update_prefix:
+            prefix = response['prefix']
+            self.prefix_path().child(prefix).set(guild_name)
+
+        # Now we get the members and return a number for change between this and last iteration
+        members = map(lambda m: GuildMember.from_data(m).to_dict_item(), response['members'])
+        memberdict = dict(members)
+        memberdict_old = self.guild_path().child(guild_name).child('members').get().val() or {}
+        num_changes = len(memberdict.keys() | memberdict_old.keys()) - len(memberdict.keys() & memberdict_old.keys())
+        self.guild_path().child(guild_name).child('members').set(memberdict)
+
+        # Record this update
+        if num_changes == 0:
+            no_diff_days = self.guild_path().child(guild_name).child('no_diff_days').get().val()
+            no_diff_days += 1
+        else:
+            no_diff_days = 0
+
+        interval = self.guild_path().child(guild_name).child('interval').get().val()
+        next_interval = self.calc_next_interval(td(seconds=interval), no_diff_days, num_changes)
+        next_update = dt.utcnow() + next_interval
+
+        self.guild_path().child(guild_name).update({
+            "interval": next_interval.total_seconds(),
+            "no_diff_days": no_diff_days,
+            "next_update": next_update.timestamp()
+        })
+
+        return next_update.timestamp()
---
+++ bot/bot/utils/wynn.py
@@ -0,0 +1,42 @@
+from datetime import datetime as dt
+from dateutil import parser as dtparser
+
+RANKS = [
+    'RECRUIT',
+    'RECRUITER',
+    'CAPTAIN',
+    'STRATEGIST',
+    'CHIEF',
+    'OWNER'
+]
+
+
+class GuildMember:
+    def __init__(self,
+                 uuid: str,
+                 name: str,
+                 rank: int,
+                 joined: dt,
+                 contributed: int,
+                 **_):
+        self.uuid = uuid
+        self.name = name
+        self.rank = rank
+        self.joined = joined
+        self.contributed = contributed
+
+    @classmethod
+    def from_data(cls, data):
+        if data['rank']:
+            data['rank'] = RANKS.index(data['rank'])
+        if data['joined']:
+            data['joined'] = dtparser.parse(data['joined'])
+        return cls(**data)
+
+    # uuid -> { name, rank: int, joined: float (timestamp), contributed: int }
+    def to_dict_item(self):
+        k = self.uuid
+        v = vars(self)
+        del v['uuid']
+        v['joined'] = v['joined'].timestamp()
+        return k, v
