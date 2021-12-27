436d0b4d1af766ef86d1d86e1cd60f9e276b92de
unrealintegers <47798345+unrealintegers@users.noreply.github.com>
2021-12-27 21:22:23 +1100

    fix deleted guilds not being deleted properly

--- bot/bot/tasks/guild.py
+++ bot/bot/tasks/guild.py
@@ -128,8 +128,9 @@ class GuildUpdater(BotTask):
 
         # Check for error: guild not found
         if response.get("error") == "Guild not found":
-            # The guild was deleted, so we add it to deleted_guilds
+            # The guild was deleted, so we add it to deleted_guilds and remove it from guilds
             last_info = self.guild_path().child(guild_name).get().val()
+            self.guild_path().child(guild_name).set([])
             last_info['deleted'] = dt.now().timestamp()
             self.deleted_path().child(guild_name).set(last_info)
             return
