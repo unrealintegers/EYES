53f06f3ee1b7478459f39d425d9b06ebe5afb77d
unrealintegers <47798345+unrealintegers@users.noreply.github.com>
2022-01-10 21:40:49 +1100

    store playtime as float instead

--- bot/bot/tasks/playtime.py
+++ bot/bot/tasks/playtime.py
@@ -13,5 +13,5 @@ class PlayerPlaytimeUpdater(BotTask):
 
     def update(self, players: List[str]):
         now = int(dt.utcnow().timestamp())
-        update_dict = {f'{k}/{now}/': True for k in players}
+        update_dict = {f'{k}/{now}/': 0.5 for k in players}
         self.path().update(update_dict)
