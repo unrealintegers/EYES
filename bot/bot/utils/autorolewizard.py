8e0de9b68e14d1b65156353b35734cfe4efd086b
unrealintegers <47798345+unrealintegers@users.noreply.github.com>
2022-03-30 01:59:18 +1100

    fix autorole

--- bot/bot/utils/autorolewizard.py
+++ bot/bot/utils/autorolewizard.py
@@ -110,8 +110,7 @@ class AutoroleWizard:
         else:
             channel_id = int(channel_match.group(1))
             channel = await interaction.guild.fetch_channel(channel_id)
-            await channel.send(content=self.sample.content, embeds=self.sample.embeds,
-                               view=View(*[c for r in self.sample.components for c in r.children]))
+            await channel.send(content=self.sample.content, embeds=self.sample.embeds, view=self.sample_view)
 
         await msg.delete()
         await interaction.delete_original_message()
