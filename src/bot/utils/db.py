560d7d1a4f09849c4fa5011b705ae831c44ec2dc
unrealintegers <47798345+unrealintegers@users.noreply.github.com>
2023-09-08 00:44:31 +1000

    change to use connection pool instead of connection

--- src/bot/utils/db.py
+++ src/bot/utils/db.py
@@ -2,47 +2,45 @@ import os
 
 import psycopg
 import psycopg.rows
-
+from psycopg_pool import AsyncConnectionPool
 
 class DatabaseManager:
     def __init__(self):
-        self.conn = None
+        self.pool = None
 
     async def init(self):
-        self.conn = await psycopg.AsyncConnection.connect(os.getenv("DB_CREDS"), autocommit=True)
+        async def configure(conn):
+            await conn.set_autocommit(True)
 
-    # Implement some behaviour of the connection class
-    async def __aenter__(self):
-        return self.conn.__enter__()
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        return self.conn.__exit__(exc_type, exc_val, exc_tb)
-
-    def __getattr__(self, item):
-        return getattr(self.conn, item)
+        self.pool = AsyncConnectionPool(os.getenv("DB_CREDS"), configure=configure)
 
     async def copy_to(self, query, rows):
-        async with self.conn.cursor() as cur:
-            async with cur.copy(query) as copy:
-                for row in rows:
-                    await copy.write_row(row)
+        async with self.pool.connection() as conn:
+            async with conn.cursor() as cur:
+                async with cur.copy(query) as copy:
+                    for row in rows:
+                        await copy.write_row(row)
 
     async def run(self, query, vars=tuple()):
-        async with self.conn.cursor() as cur:
-            await cur.execute(query, vars)
+        async with self.pool.connection() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute(query, vars)
 
     async def run_batch(self, query, vars=tuple()):
-        async with self.conn.cursor() as cur:
-            await cur.executemany(query, vars)
+        async with self.pool.connection() as conn:
+            async with conn.cursor() as cur:
+                await cur.executemany(query, vars)
 
     async def fetch_tup(self, query, vars=tuple()):
-        async with self.conn.cursor() as cur:
-            await cur.execute(query, vars)
+        async with self.pool.connection() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute(query, vars)
 
-            return await cur.fetchall()
+                return await cur.fetchall()
 
     async def fetch_dict(self, query, vars=tuple()):
-        async with self.conn.cursor(row_factory=psycopg.rows.dict_row) as cur:
-            await cur.execute(query, vars)
+        async with self.pool.connection() as conn:
+            async with conn.cursor(row_factory=psycopg.rows.dict_row) as cur:
+                await cur.execute(query, vars)
 
-            return await cur.fetchall()
+                return await cur.fetchall()
